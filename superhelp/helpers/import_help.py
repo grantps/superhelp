d9c48f43e30fc44c4e1f487a06b791a1f640572a
Grant Paton-Simpson <grant@sofastatistics.com>
2020-06-01 20:24:11 +1200

    Complete help on pathlib and on absolute importing

--- superhelp/helpers/import_help.py
+++ superhelp/helpers/import_help.py
@@ -58,29 +58,17 @@ def internal_imports(blocks_dets, *, repeat=False, **_kwargs):
         1) __Use absolute importing__ e.g. packagefolder.subfolder.modname
 
         2) If absolute importing is not working, don't do "crazy random stuff"
-        from Stack Overflow - instead, __check sys.path is correct__. Look at
-        the value for sys.path inside the module which is failing to import the
-        internal module. If it doesn't contain the path to the folder
-        surrounding / outside / above the package folder, that is the cause of
-        the problem and that is what you need to fix.
+        from Stack Overflow - instead, __check `sys.path` is correct__. Look at
+        the value for `sys.path` inside the module which is failing to import
+        the internal module. If `sys.path` doesn't contain the path to the
+        folder surrounding / outside / above the package folder, that is the
+        cause of the problem and that is what you need to fix.
         """)
     main_msg = (
         layout("""\
 
-        Importing internal modules when your code is in a folder structure is
-        simple if you follow some basic rules:
-
-        1) __Use absolute importing__ e.g. packagefolder.subfolder.modname
-
-        2) If absolute importing is not working, don't do "crazy random stuff"
-        from Stack Overflow - instead, __check sys.path is correct__. Look at the
-        value for sys.path inside the module which is failing to import the
-        internal module. If it doesn't contain the path to the folder
-        surrounding / outside / above the package folder, that is the cause of
-        the problem and that is what you need to fix.
-
         For example, imagine we have a package called `superhelp` with a module
-        `conf.py` and subfolder `displayers`. Iside the subfolder is a module
+        `conf.py` and subfolder `displayers`. Inside the subfolder is a module
         called `html_displayer`. The package is inside a folder called
         `/home/g/projects`.
         """)
@@ -119,9 +107,26 @@ def internal_imports(blocks_dets, *, repeat=False, **_kwargs):
         Only hack `sys.path` inside your code as a last resort.
         """)
     )
+    extra_msg = layout("""\
+
+    Some people like the elegance of the dot notation of relative importing but
+    you need to know what you're doing if you use it. Relative importing only
+    really makes sense if you're making code to be run as a library.
+
+    Any modules in your library you want to run as scripts while developing
+    (e.g. to get access to line-by-line debugging from your IDE) must use
+    absolute importing for internal modules they reference AND sys.path must
+    include the surrounding folder (i.e. just the usual rules explained
+    earlier).
+
+    Note – other modules you don’t want to run directly can continue to use
+    relative importing – even if they are imported (absolutely) from modules you
+    will be running directly as scripts.
+    """)
 
     message = {
         conf.BRIEF: title + brief_msg,
         conf.MAIN: title + brief_msg + main_msg,
+        conf.EXTRA: extra_msg,
     }
     return message
